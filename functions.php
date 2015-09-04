e2f757ba9d998f0ec9a8d77b399fc3d2b97416ad
Ahmed Khateeb <khateeb321@gmail.com>
2015-09-04 05:23:27 -0700

    added featured image to posts background, but it sucks

--- functions.php
+++ functions.php
@@ -6,3 +6,14 @@
 
 	add_action('init','register_second_menu');
 ?>
+
+
+	<!--
+	<nav class="nav-primary" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+	  <div class="wrap">
+	    <ul id="menu-secondary-navigation" class="menu genesis-nav-menu menu-primary">
+	      <?php wp_nav_menu(array( 'theme_location' => 'Categories' )); ?>
+	    </ul>
+	  </div>
+	</nav>
+	-->
